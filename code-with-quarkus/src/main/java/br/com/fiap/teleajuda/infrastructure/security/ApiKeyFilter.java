ba8086477f128d7c239af1f17944a65a67c37e1c
Julia de Altino <julia.correa.souza@gmail.com>
2025-11-11 22:25:20 -0300

    colocando sugestao do git para arrumar cors

--- code-with-quarkus/src/main/java/br/com/fiap/teleajuda/infrastructure/security/ApiKeyFilter.java
+++ code-with-quarkus/src/main/java/br/com/fiap/teleajuda/infrastructure/security/ApiKeyFilter.java
@@ -27,25 +27,15 @@ public class ApiKeyFilter implements ContainerRequestFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        // 1. Bypass para o Swagger
-        if (requestContext.getUriInfo().getPath().contains("swagger") || requestContext.getUriInfo().getPath().contains("q/swagger-ui")) {
-            return;
-        }
-
-        // 2. [LINHA MAIS IMPORTANTE] Bypass para requisições OPTIONS (Preflight do CORS)
-        // Se este "if" não estiver aqui, o CORS VAI FALHAR.
+        // SOLUÇÃO: Ignorar a verificação para o método OPTIONS (o preflight CORS)
         if (requestContext.getMethod().equalsIgnoreCase("OPTIONS")) {
             return;
         }
 
-        // 3. Validação da API Key
-        String apiKey = requestContext.getHeaderString("X-API-KEY");
-        if (apiKey == null || !apiKeyValidator.isValid(apiKey)) {
-            requestContext.abortWith(
-                    Response.status(Response.Status.UNAUTHORIZED)
-                            .entity("API Key inválida ou ausente.")
-                            .build());
-
+        final String apiKey = requestContext.getHeaderString(API_KEY_HEADER);
+        if(!apiKeyValidator.isValid(apiKey)) {
+            throw new NotAuthorizedException("Invalid API key");
         }
+
     }
 }
